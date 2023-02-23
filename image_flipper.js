73b1f0b5ddfbfb57e3f1a19a7841ec65c4a1caad
Gerald Bartlett <gerald.bartlett@hotmail.com>
2023-02-23 20:23:33 -0330

    first commit

---
+++ image_flipper.js
@@ -0,0 +1,59 @@
+ // document.body.style.backgroundImage = "url('img_tree.png')";
+    
+ myFunction = () => {            
+    document.body.style.backgroundImage = "url('/images/Alberta.jpg')";
+}
+
+myFunction2 = () => {
+    document.body.style.backgroundImage = "url('/images/BC.png')";
+}
+ myFunction3 = () => {
+    document.body.style.backgroundImage = "url('/images/Manitoba.jpg')";
+  
+}
+
+myFunction4 = ()=> {
+    document.body.style.backgroundImage = "url('/images/Newbrunswick.jpg')";
+    document.body.style.backgroundColor = "pink";
+}
+
+myFunction5 = () => {
+  
+    document.body.style.backgroundImage = "url('/images/Nfld.jpg')";
+}
+myFunction6 = () => {
+    document.body.style.backgroundImage = "url('/images/NWT.jpg')";
+
+}
+myFunction7 = () => {
+    document.body.style.backgroundImage = "url('/images/nova_scotia.jpg')";
+ 
+}
+myFunction9 = () => {
+    document.body.style.backgroundImage = "url('/images/Nunavut.jpg')";
+ 
+}
+myFunction10 = () => {
+    document.body.style.backgroundImage = "url('/images/Ontario.avif')";
+ 
+}
+myFunction11 = () => {
+    document.body.style.backgroundImage = "url('/images/PEI.webp')";
+ 
+}
+myFunction12 = () => {
+    document.body.style.backgroundImage = "url('/images/Quebec.jpg')";
+ 
+}
+myFunction13 = () => {           
+    document.body.style.backgroundImage = "url('/images/Saskatchewan.jpg')";
+    document.body.style.backgroundColor = "yellow";
+}
+myFunction14 = () => {           
+    document.body.style.backgroundImage = "url('/images/Yukon.jpg')";
+}
+
+myFunction8 = () => {           
+    document.body.style.backgroundImage = "url('/images/canada-map.jpg')";
+    document.body.style.backgroundColor = "rgb(0,191,255)";
+}
