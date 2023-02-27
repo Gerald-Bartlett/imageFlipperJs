c26a1304d67d354dc1138badfaeba569b8d129a4
Gerald Bartlett <gerald.bartlett@hotmail.com>
2023-02-27 18:58:39 -0330

    Update image_flipper.js

--- image_flipper.js
+++ image_flipper.js
@@ -1,59 +1,61 @@
- // document.body.style.backgroundImage = "url('img_tree.png')";
+
     
  myFunction = () => {            
-    document.body.style.backgroundImage = "url('/images/Alberta.jpg')";
+    document.body.style.backgroundImage = "url('images/Alberta.jpg')";
+  document.body.style.backgroundColor = "green";
 }
 
 myFunction2 = () => {
-    document.body.style.backgroundImage = "url('/images/BC.png')";
+    document.body.style.backgroundImage = "url('images/BC.png')";
+  document.body.style.backgroundColor = "light-blue";
 }
  myFunction3 = () => {
-    document.body.style.backgroundImage = "url('/images/Manitoba.jpg')";
-  
+    document.body.style.backgroundImage = "url('images/Manitoba.jpg')";
+   document.body.style.backgroundColor = "purple";
 }
 
 myFunction4 = ()=> {
-    document.body.style.backgroundImage = "url('/images/Newbrunswick.jpg')";
-    document.body.style.backgroundColor = "pink";
+    document.body.style.backgroundImage = "url('images/Newbrunswick.jpg')";
+    document.body.style.backgroundColor = "beige";
 }
 
-myFunction5 = () => {
-  
-    document.body.style.backgroundImage = "url('/images/Nfld.jpg')";
+myFunction5 = () => {  
+    document.body.style.backgroundImage = "url('images/Nfld.jpg')";
+  document.body.style.backgroundColor = "pink";
 }
 myFunction6 = () => {
-    document.body.style.backgroundImage = "url('/images/NWT.jpg')";
-
+    document.body.style.backgroundImage = "url('images/NWT.jpg')";
+ document.body.style.backgroundColor = "green";
 }
 myFunction7 = () => {
-    document.body.style.backgroundImage = "url('/images/nova_scotia.jpg')";
- 
+    document.body.style.backgroundImage = "url('images/nova_scotia.jpg')";
+  document.body.style.backgroundColor = "dark-green";
 }
 myFunction9 = () => {
-    document.body.style.backgroundImage = "url('/images/Nunavut.jpg')";
- 
+    document.body.style.backgroundImage = "url('images/Nunavut.jpg')";
+  document.body.style.backgroundColor = "grey";
 }
 myFunction10 = () => {
-    document.body.style.backgroundImage = "url('/images/Ontario.avif')";
- 
+    document.body.style.backgroundImage = "url('images/Ontario.avif')";
+  document.body.style.backgroundColor = "pink";
 }
 myFunction11 = () => {
-    document.body.style.backgroundImage = "url('/images/PEI.webp')";
- 
+    document.body.style.backgroundImage = "url('images/PEI.webp')";
+  document.body.style.backgroundColor = "brown";
 }
 myFunction12 = () => {
-    document.body.style.backgroundImage = "url('/images/Quebec.jpg')";
- 
+    document.body.style.backgroundImage = "url('images/Quebec.jpg')";
+  document.body.style.backgroundColor = "green";
 }
 myFunction13 = () => {           
-    document.body.style.backgroundImage = "url('/images/Saskatchewan.jpg')";
+    document.body.style.backgroundImage = "url('images/Saskatchewan.jpg')";
     document.body.style.backgroundColor = "yellow";
 }
 myFunction14 = () => {           
-    document.body.style.backgroundImage = "url('/images/Yukon.jpg')";
+    document.body.style.backgroundImage = "url('images/Yukon.jpg')";
 }
 
 myFunction8 = () => {           
-    document.body.style.backgroundImage = "url('/images/canada-map.jpg')";
+    document.body.style.backgroundImage = "url('images/canada-map.jpg')";
     document.body.style.backgroundColor = "rgb(0,191,255)";
 }
